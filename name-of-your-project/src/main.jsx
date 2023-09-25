bfef130cadd23d1bcfaa67828f4dbb9263b3b5f1
Alex Marquez <marquez798@gmail.com>
2023-09-24 21:58:14 -0500

    finished setting up routes

--- name-of-your-project/src/main.jsx
+++ name-of-your-project/src/main.jsx
@@ -10,11 +10,15 @@ import Root, {
 } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact, {
-  loader as contactLoader,
+  loader as contactLoader, action as contactAction,
 } from "./routes/contact";
 
 import EditContact, {  action as editAction,
 } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
+
+
 
 
 
@@ -27,15 +31,35 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          /* the rest of the routes */
+        ],
+      },
+      { index: true, element: <Index /> },
+      {
         path: "contacts/:contactId",
         element: <Contact />,
         loader:contactLoader,
+        action: contactAction,
       },
       {
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ]
   },
